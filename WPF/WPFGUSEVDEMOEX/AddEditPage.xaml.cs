df03a974f0cb7c449f755dedba88f932779f07ad
V1suG <Vesug1997@gmail.com>
2026-03-06 19:25:34 +0300

    первый

---
+++ WPF/WPFGUSEVDEMOEX/AddEditPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFGUSEVDEMOEX.ClassesForAll;
+
+namespace WPFGUSEVDEMOEX
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Partners_product_import_ _currentPurchase = new Partners_product_import_();
+        public AddEditPage(Partners_product_import_ selectedPurchase)
+        {
+            InitializeComponent();
+           if (selectedPurchase != null)
+                _currentPurchase = selectedPurchase;
+            DataContext = selectedPurchase;
+            ComboProducts.ItemsSource = DemoExamEntities.GetContext().Product_import_.ToList();
+            ComboPartners.ItemsSource = DemoExamEntities.GetContext().Partners_import_.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder erros = new StringBuilder();
+            if (_currentPurchase.Product_import_ == null)
+                erros.AppendLine("Выберите продукт");
+            if (_currentPurchase.Partners_import_ == null)
+                erros.AppendLine("Выберите парнёра");
+            if (_currentPurchase.quantity_import <= 0)
+                erros.AppendLine("Цена должна быть < 0");
+            if (_currentPurchase.Date_import == null)
+                erros.AppendLine("Выберите дату");
+            if (erros.Length > 0)
+            {
+                MessageBox.Show(erros.ToString());
+                return;
+            }
+            if (DemoExamEntities.GetContext().Entry(_currentPurchase).State == EntityState.Detached)
+            {
+                DemoExamEntities.GetContext().Partners_product_import_.Add(_currentPurchase);
+            }
+            try
+            {
+                DemoExamEntities.GetContext().SaveChanges();
+                MessageBox.Show("Данные сохранены");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = "Ошибка: " + ex.Message;
+                while (ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                    errorMessage += "\nInnerException: " + ex.Message;
+                }
+                MessageBox.Show(errorMessage);
+            }
+        }
+    }
+}
---
+++ WPF/WPFGUSEVDEMOEX/PurchasesPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFGUSEVDEMOEX.ClassesForAll;
+using System.Data.Entity;
+
+namespace WPFGUSEVDEMOEX
+{
+    /// <summary>
+    /// Логика взаимодействия для PurchasesPage.xaml
+    /// </summary>
+    public partial class PurchasesPage : Page
+    {
+        public PurchasesPage()
+        {
+            InitializeComponent();
+            DGridPurchases.ItemsSource = DemoExamEntities.GetContext().Partners_product_import_.Include(p => p.Product_import_).Include(p => p.Partners_import_).ToList();
+        }
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItem = (sender as Button).DataContext as Partners_product_import_;
+            var context = DemoExamEntities.GetContext();
+            var loadedItem = context.Partners_product_import_.Include(p => p.Product_import_).FirstOrDefault(p => p.partner_products_import_ID == selectedItem.partner_products_import_ID);
+            Manager.MainFrame.Navigate(new AddEditPage(loadedItem));
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var purchasesForRemoving = DGridPurchases.SelectedItems.Cast<Partners_product_import_>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить следущие {purchasesForRemoving.Count()} элементов?", "Внимание!",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    DemoExamEntities.GetContext().Partners_product_import_.RemoveRange(purchasesForRemoving);
+                    DemoExamEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                DemoExamEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridPurchases.ItemsSource = DemoExamEntities.GetContext().Partners_product_import_.Include(p => p.Product_import_).Include(p => p.Partners_import_).ToList();
+            }
+        }
+    }
+}
